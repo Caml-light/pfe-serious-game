781f4ea7a0f31e7fef4b806ff6f51913e18c2b31
Thomas Lefranc <tl.thomas.lefranc@gmail.com>
2015-12-08 10:47:23 +0100

    Premier test unitaire pour test avec le git.

---
+++ Assets/Editor/UT_Indicator.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using System;
+using Unity;
+
+[TestFixture]
+public class UT_Indicator
+{
+    Indicator ind;
+
+
+    [SetUp]
+    public void SetUp()
+    {
+
+        ind = Substitute.For<Indicator>("Test", 100.0, 0.99, 50.0, 1.0);
+    }
+
+    [Test]
+    public void TestVariables()
+    {
+        Assert.That(ind.Value == 100.0);
+        Assert.That(ind.Name == "Test");
+    }
+
+  
+
+    [TearDown]
+    public void TearDown()
+    {
+        ind = null;
+    }
+}
