b50fba3e039027a7434fc56b729d1798eb050b2f
YoannUrvoy <yoann.urvoy@gmail.com>
2015-12-17 20:09:50 +0100

    Surbrillance
    Ajout de le surbrillance pour chaque pays lors du passage de la souris.
    Suppression de l'ancienne fonctionnalité de surbrillance

--- Assets/Scripts/Classes/Continent.cs
+++ Assets/Scripts/Classes/Continent.cs
@@ -16,10 +16,14 @@ public class Continent : MonoBehaviour {
     public Indicator biodiversity;
     public string _Name;
 
+    private Animator surbrillance; 
    
 
     // Use this for initialization
     void Start () {
+
+        surbrillance = GetComponent<Animator>(); // permet de gérer la surbrillance (on lui attribu l'animator)
+
         Debug.Log(Name +" start");
         pop = new Indicator("Population", 100.0, 0.99, 50.0, 1.0);
         foodNeed = new Indicator("Hunger", 100.0, 0.99, 50.0, 1.0);
@@ -41,10 +45,8 @@ public class Continent : MonoBehaviour {
     void OnMouseEnter() // Lorsque l'on passe la souris sur le continent
     {
         if (GameManager.instance.isZoomed == false && GameManager.instance.isZoomFinished) // On vérifie que l'on est en vue globale (et non en vue zoomée), et que l'on n'est pas en pleine annimation de zoom
-        {
-            Vector3 temp = new Vector3(0, 0, -120); // On créé un vecteur de translation pour faire avancer l'image du continent au premier plan (devant celle du planisphère)
-            transform.position += temp; // On l'applique à l'objet
-            /* ça sera supprimé lors de l'ajout des vraies annimaions */
+        { 
+            surbrillance.SetBool("isMouseOver", true);
         }
     }
 
@@ -52,9 +54,7 @@ public class Continent : MonoBehaviour {
     {
         if (GameManager.instance.isZoomed == false && GameManager.instance.isZoomFinished) // On vérifie que l'on est en vue zoomé, et que l'on n'est pas en pleine annimation de zoom
         {
-            Vector3 temp = new Vector3(0, 0, 120); // On créé un veteur de translation pour le faire passer derière le planisphère
-            transform.position += temp;
-            /* ça sera supprimé lors de l'ajout des vraies annimaions */
+            surbrillance.SetBool("isMouseOver", false);
         }
     }
 
@@ -67,20 +67,15 @@ public class Continent : MonoBehaviour {
 
         if (GameManager.instance.isZoomFinished && !GameManager.instance.isZoomed) // Si on n'est pas en pleine annimation et que on est en vue globale
         {
+            surbrillance.SetBool("isMouseOver", false);
+
             GameManager.instance.ContinentSelected = this; // On attribu au GameManager le continent sélectionné 
             GameManager.instance.isZoomed = !GameManager.instance.isZoomed; // On annonce que on passe en vue zoomée
             GameManager.instance.isZoomFinished = false; // On indique que l'nnimation commence
 
-            if (transform.position.z < 2) // correction d'un bug
-            { 
-                Vector3 temp = new Vector3(0, 0, 120); // ça sera supprimé lors de l'ajout des vraies annimaions
-                transform.position += temp;
-            }
-
             GameManager.instance.DisplayContinentIndicators(); // On affiche les indicateurs
 
             //INSTUCTIONS lors d'un clic sur le continent ici....
-
         }
         
 
