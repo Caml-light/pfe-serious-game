a32e81e59c994d0837a175dd999adb309fa3d61c
Thomas Lefranc <tl.thomas.lefranc@gmail.com>
2016-02-11 22:13:56 +0100

    Bonus fait maison par Thomas Lefranc (nécessite de lancer le jeu à partir du .exe !)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GameLaunch(string name)
+    {
+        if (name.Equals("new"))
+        {
+            Application.LoadLevel(1);
+        }
+        else if (name.Equals("exit"))
+        {
+            Application.Quit();
+        }
+    }
+}
